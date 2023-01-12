d107203ba192a8ff8880065e8814f7e4a1dd6860
Cayley <cayley@dr.com>
2023-01-12 16:24:54 +0900

    4. GUI

--- day01.py
+++ day01.py
@@ -1,14 +1,5 @@
-# Dictionary
-days = {'Jan': 31, 'Feb': 28, 'Mar': 31, 'Apr': 30, 'May': 31, 'Jun': 30,
-        'Jul': 31, 'Aug': 31, 'Sep': 30, 'Oct': 31, 'Nov': 30, 'Dec': 31}
+import tkinter as tk
 
-month = 'Jan'
-
-# old style
-print('%s has %d days' % (month, days[month]))
-
-# format function
-print('{0} has {1} days'.format(month, days[month]))
-
-# using f-string
-print(f'{month} has {days[month]} days')
+win = tk.Tk()
+win.geometry('400x300')
+win.mainloop()
